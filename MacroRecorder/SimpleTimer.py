68a61b38f70d1b5e7ecc69832bbe785f95faab62
TG892071 <thomas.m.h.gibson@gmail.com>
2023-06-01 20:20:33 +0100

    Tidied SimpleTimer Class

--- MacroRecorder/SimpleTimer.py
+++ MacroRecorder/SimpleTimer.py
@@ -9,7 +9,6 @@ class SimpleTimer:
         """Start a new timer"""
         if self._start_time is not None:
             raise TimerError(f"Timer is running. Use .stop() to stop it")
-
         self._start_time = time.perf_counter()
 
     def current_time(self):
@@ -23,6 +22,4 @@ class SimpleTimer:
         """Stop the timer, and report the elapsed time"""
         if self._start_time is None:
             raise TimerError(f"Timer is not running. Use .start() to start it")
-
-        elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
